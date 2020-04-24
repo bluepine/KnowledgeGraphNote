0105f24de0cf91fff2c051b6c511c666d9186b08
Song Wei <1983.song.wei@gmail.com>
2020-04-24 09:43:42 -0400

    Files generated by PkgTemplates

---
+++ src/KnowledgeGraphNote.jl
@@ -0,0 +1,5 @@
+module KnowledgeGraphNote
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KnowledgeGraphNote
+using Test
+
+@testset "KnowledgeGraphNote.jl" begin
+    # Write your own tests here.
+end
