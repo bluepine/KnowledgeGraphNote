928ddb2afffac72dcfd76a4089e2f6b4b71e3409
Song Wei <1983.song.wei@gmail.com>
2020-04-24 18:23:38 -0400

    introduce KnowledgeGraph struct to hold knowledge graph in memory.

--- src/KnowledgeGraphNote.jl
+++ src/KnowledgeGraphNote.jl
@@ -12,6 +12,12 @@ struct Concept
     text::String
 end
 
+struct KnowledgeGraph
+    graph::SimpleDiGraph
+    nametoid::Dict{String,Int} # noramlized concept name => vertex id in graph
+    idtoname::Array{String} # vertex id in graph => noramlized concept name
+end
+
 function print_concepts(concepts)
     if isnothing(concepts)
         return
@@ -29,7 +35,33 @@ end
 
 normalize_concept_name(name) = lowercase(strip(name))
 
+get_concept_index(concepts) = Dict([(normalize_concept_name(concept.name), findfirst(x->x.name == concept.name, concepts)) for concept in concepts])
+
+function get_digraph(concepts)
+    g = SimpleDiGraph(length(concepts))
+    concept_index = get_concept_index(concepts)
+    for (index, concept) in enumerate(concepts)
+        if length(concept.dependency) > 0
+            for prerequisite in concept.dependency
+                nprerequisite = normalize_concept_name(prerequisite)
+                if haskey(concept_index, nprerequisite)
+                    # ignore missing concepts for now
+                    add_edge!(g, index, concept_index[nprerequisite])
+                end
+            end
+        end
+    end
+    return (g, concept_index, [normalize_concept_name(concept.name) for concept in concepts])
+end
+
+# use an analyzer to hold the knowledge graph in memory so we don't have to recreate it as long as the concepts are not changed.
+function init_knowledge_graph(concepts)
+    g, nametoid, idtoname = get_digraph(concepts)
+    return KnowledgeGraph(g, nametoid, idtoname)
+end
+
 get_known_concepts(concepts) = unique(normalize_concept_name.(getfield.(concepts, :name)))
+
 
 function get_duplicate_concepts(concepts)
     known_concepts = get_known_concepts(concepts)
@@ -47,31 +79,11 @@ function missing_concepts(concepts)
     return ret
 end
 
-get_concept_index(concepts) = Dict([(normalize_concept_name(concept.name), findfirst(x->x.name == concept.name, concepts)) for concept in concepts])
-
-function get_digraph(concepts)
-    g = SimpleDiGraph(length(concepts))
-    concept_index = get_concept_index(concepts)
-    for (index, concept) in enumerate(concepts)
-        if length(concept.dependency) > 0
-            for prerequisite in concept.dependency
-                nprerequisite = normalize_concept_name(prerequisite)
-                if haskey(concept_index, nprerequisite)
-                    # ignore missing concepts for now
-                    add_edge!(g, index, concept_index[nprerequisite])
-                end
-            end
-        end
-    end
-    return (g, concept_index)
+function find_cycles(kg::KnowledgeGraph)
+    #    savegraph("test.gml", kg.graph, "graph_name", GMLFormat())
+    cycles = simplecycles(kg.graph)
+    return [kg.idtoname[cycle] for cycle in cycles]
 end
 
-function find_cycles(concepts)
-    g, concept_index = get_digraph(concepts)
-    #    savegraph("test.gml", g, "graph_name", GMLFormat())
-    cycles = simplecycles(g)
-    return [getfield.(concepts[cycle], :name) for cycle in cycles]
-end
-
-export print_concepts, missing_concepts, get_duplicate_concepts, get_known_concepts, find_cycles
+export init_analyer, print_concepts, missing_concepts, get_duplicate_concepts, get_known_concepts, find_cycles, init_knowledge_graph
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ include("notes.jl")
     @test Set(["affinely independent", "measure", "limit point", "empty set", "power set", "neighborhood", "set closure", "compact set"]) == Set(KnowledgeGraphNote.missing_concepts(math_notes))
     @test Dict("a"=>2) == KnowledgeGraphNote.get_duplicate_concepts(duplicate_notes)
     @test Set(["a", "b"]) == Set(KnowledgeGraphNote.get_known_concepts(duplicate_notes))
-    @test length(KnowledgeGraphNote.find_cycles(math_notes)) == 0
-    @test [["a", "b", "c"]] == KnowledgeGraphNote.find_cycles(cyclic_notes)
+    mathkg = KnowledgeGraphNote.init_knowledge_graph(math_notes)
+    @test length(KnowledgeGraphNote.find_cycles(mathkg)) == 0
+    cyclickg = KnowledgeGraphNote.init_knowledge_graph(cyclic_notes)
+    @test [["a", "b", "c"]] == KnowledgeGraphNote.find_cycles(cyclickg)
 end
