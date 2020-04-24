a28a1572a7669f92f6369edd3026ba98b26b9303
Song Wei <1983.song.wei@gmail.com>
2020-04-24 11:09:30 -0400

    init

--- src/KnowledgeGraphNote.jl
+++ src/KnowledgeGraphNote.jl
@@ -1,5 +1,58 @@
 module KnowledgeGraphNote
 
-greet() = print("Hello World!")
+using LightGraphs
 
+struct Concept
+    name::String
+    link::String    
+    dependency::Array{String, 1}
+    text::String
+end
+
+function print_concepts(concepts)
+    if isnothing(concepts)
+        return
+    end
+    for concept in concepts
+        println("name: $(concept.name)")
+        println("link: $(concept.link)")
+        if length(concept.dependency) > 0
+            println("prerequisites: $(concept.dependency)")
+        end
+        println(concept.text)
+        println("")
+    end
+end
+
+normalize_concept_name(name) = lowercase(strip(name))
+
+get_known_concepts(concepts) = unique(normalize_concept_name.(getfield.(concepts, :name)))
+
+function get_duplicate_concepts(concepts)
+    known_concepts = get_known_concepts(concepts)
+    concept_occurrences = [(concept,count(c->normalize_concept_name(c.name)==concept, concepts)) for concept in known_concepts]
+    duplicate_occurrences = Dict(filter(item -> item[2] > 1, concept_occurrences))
+    return duplicate_occurrences
+end
+
+function missing_concepts(concepts)
+    known_concepts = get_known_concepts(concepts)
+    dependencies = reduce(concepts; init = Set()) do acc, concept
+        acc = union(acc, Set(concept.dependency))
+    end
+    ret = setdiff(normalize_concept_name.(dependencies), known_concepts)    
+    return ret
+end
+
+# function digraph(concepts)
+#     g = SimpleDiGraph(length(concepts))
+
+# end
+
+# function find_cycles(concepts)
+
+#     savegraph("test.gml", g, "graph_name", GraphIO.GML.GMLFormat())    
+# end
+
+export print_concepts, missing_concepts, get_duplicate_concepts, get_known_concepts
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using KnowledgeGraphNote
 using Test
+include("notes.jl")
 
 @testset "KnowledgeGraphNote.jl" begin
-    # Write your own tests here.
+    println("testing functions: $(names(KnowledgeGraphNote)[2:end])")    
+    #    KnowledgeGraphNote.print_concepts(math_concepts)
+    @test Set(["affinely independent", "measure", "limit point", "empty set", "power set", "neighborhood", "set closure", "compact set"]) == Set(KnowledgeGraphNote.missing_concepts(math_concepts))
+    @test Dict("a"=>2) == KnowledgeGraphNote.get_duplicate_concepts(duplicate_notes)
+    @test Set(["a", "b"]) == Set(KnowledgeGraphNote.get_known_concepts(duplicate_notes))
 end
