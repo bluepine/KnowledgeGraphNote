fd8a8db2f81da7ad7e58ef3dac3af3cb189ad365
Song Wei <1983.song.wei@gmail.com>
2020-04-25 22:48:39 -0400

    explain file output code a bit more.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ include("notes.jl")
 
 @testset "KnowledgeGraphNote.jl" begin
 
-######################### testing library api
+    ######################### testing library api
     # KnowledgeGraphNote.print_concepts(math_notes)
     @test Set(KnowledgeGraphNote.get_missing_concepts(math_notes)) == Set(["affinely independent", "measure", "limit point", "empty set", "power set", "neighborhood", "set closure", "compact set"])
     @test KnowledgeGraphNote.get_duplicate_concepts(duplicate_notes) == Dict("a"=>2)
@@ -21,13 +21,14 @@ include("notes.jl")
     target = "borel sigma-algebra"    
     @test mathkg.idtoname[KnowledgeGraphNote.generate_learning_plan(mathkg, target)] == ["set", "sigma-algebra", "metric", "metric space", "open set", "closed set", "topological space", "borel sigma-algebra"]
 
-    # # it's not a good idea to write files in unit tests. i'll have to come up with something else to unittest these two functions
+    #### the dot file format is described here: https://en.wikipedia.org/wiki/DOT_(graph_description_language). you can find a viewer from there.
+    #### it's not a good idea to write files in unit tests. uncommment these lines locally to see what they do.
     # KnowledgeGraphNote.export_knowledge_graph(mathkg, "./math.dot")
     # KnowledgeGraphNote.export_knowledge_graph_towards_target(mathkg, target, "./target.dot")
     # KnowledgeGraphNote.write_notes_for_target_concept_to_md_file(mathkg, math_notes, target, "./target.md")
 
     
-######################### testing library internal functions
+    ######################### testing library internal functions
     dfs_postordering_test1_kg = KnowledgeGraphNote.init_knowledge_graph(dfs_postordering_test1)
     # KnowledgeGraphNote.export_knowledge_graph(dfs_postordering_test1_kg, "./dfs_postordering_test1.dot")
     postorder = KnowledgeGraphNote.dfs_postordering(dfs_postordering_test1_kg.graph, 1)
