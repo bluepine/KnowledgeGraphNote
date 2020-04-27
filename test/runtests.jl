785308eda049db245b65d5309f8b145840198b45
Song Wei <1983.song.wei@gmail.com>
2020-04-27 15:49:02 -0400

    update unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,28 +5,31 @@ include("notes.jl")
 @testset "KnowledgeGraphNote.jl" begin
 
     ######################### testing library api
-    # KnowledgeGraphNote.print_concepts(math_notes)
-    @test Set(KnowledgeGraphNote.get_missing_concepts(math_notes)) == Set(["affinely independent", "measure", "limit point", "empty set", "power set", "neighborhood", "set closure", "compact set"])
-    @test KnowledgeGraphNote.get_duplicate_concepts(duplicate_notes) == Dict("a"=>2)
-    @test length(KnowledgeGraphNote.get_duplicate_concepts(math_notes)) == 0
-    @test Set(KnowledgeGraphNote.get_known_concepts(duplicate_notes)) == Set(["a", "b"])
+    # print_concepts(math_notes)
+    @test Set(get_missing_concepts(math_notes)) == Set(["affinely independent", "measure", "limit point", "empty set", "power set", "neighborhood", "set closure", "compact set"])
+    @test get_duplicate_concepts(duplicate_notes) == Dict("a"=>2)
+    @test length(get_duplicate_concepts(math_notes)) == 0
+    @test Set(get_known_concepts(duplicate_notes)) == Set(["a", "b"])
     
-    mathkg = KnowledgeGraphNote.init_knowledge_graph(math_notes)
-    @test length(KnowledgeGraphNote.find_cycles(mathkg)) == 0
-    cyclickg = KnowledgeGraphNote.init_knowledge_graph(cyclic_notes)
-    @test [["a", "b", "c"]] == KnowledgeGraphNote.find_cycles(cyclickg)
-    # println(KnowledgeGraphNote.generate_learning_plan(mathkg, math_notes,  "Borel measure"))
+    mathkg = init_knowledge_graph(math_notes)
+    @test length(find_cycles(mathkg)) == 0
+    cyclickg = init_knowledge_graph(cyclic_notes)
+    @test [["a", "b", "c"]] == find_cycles(cyclickg)
+    # println(generate_learning_plan(mathkg, math_notes,  "Borel measure"))
     
     
     target = "borel sigma-algebra"    
-    @test mathkg.idtoname[KnowledgeGraphNote.generate_learning_plan(mathkg, target)] == ["set", "sigma-algebra", "metric", "metric space", "open set", "closed set", "topological space", "borel sigma-algebra"]
+    @test mathkg.idtoname[generate_learning_plan(mathkg, target)] == ["set", "sigma-algebra", "metric", "metric space", "open set", "closed set", "topological space", "borel sigma-algebra"]
 
     #### the dot file format is described here: https://en.wikipedia.org/wiki/DOT_(graph_description_language). you can find a viewer from there.
     #### it's not a good idea to write files in unit tests. uncommment these lines locally to see what they do.
-    # KnowledgeGraphNote.export_knowledge_graph(mathkg, "./math.dot")
-    # KnowledgeGraphNote.export_knowledge_graph_towards_target(mathkg, target, "./target.dot")
-    # KnowledgeGraphNote.write_notes_for_target_concept_to_md_file(mathkg, math_notes, target, "./target.md")
+    # export_knowledge_graph(mathkg, "./math.dot")
+    # export_knowledge_graph_towards_target(mathkg, target, "./target.dot")
+    # write_notes_for_target_concept_to_md_file(mathkg, math_notes, target, "./target.md")
 
+    @test check_notes(duplicate_notes) == false
+    @test check_notes(math_notes) == false
+    @test check_notes(cyclic_notes) == false
     
     ######################### testing library internal functions
     dfs_postordering_test1_kg = KnowledgeGraphNote.init_knowledge_graph(dfs_postordering_test1)
